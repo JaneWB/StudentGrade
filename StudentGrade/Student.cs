78be1dd8dce583a8b5145b0685fae8a33a0f526c
JaneWB <brownjannick@att.net>
2018-09-28 12:54:50 -0500

    CG 11-2 Student Grade

---
+++ StudentGrade/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace StudentGrade
+{
+    class Student
+    {
+        public List<int> scores = new List<int>();
+        public string Name { get; set; }
+
+
+        public Student(string name)
+        {
+            Name = name;
+        }
+
+        public void AddScore(int score)
+        {
+            scores.Add(score);
+        }
+
+        public double Average()
+        {
+            //return scores.Sum() / scores.Count;
+            return scores.Average(x => x);
+
+        }
+
+
+    }
+
+
+}
